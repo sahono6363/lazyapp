97cab1bab516616090437699199146f95affac56
repe00 <repenishizimi@gmail.com>
2025-05-17 22:04:23 +0900

    ヘッダー（仮）

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,29 @@
-import { useState } from "react";
+import React from "react";
+
 
 const Header = () => {
-    return<div>いいいい</div>
+  return (
+    <header
+      className="hachi-maru-pop-regular"
+      style={{
+        backgroundColor: "lightgreen",
+        color: "black",
+        padding: "5px",
+        textAlign: "center",
+        fontSize: "25px",
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <span style={{ marginLeft: "10px" }}>Someday List</span>
+      <div>
+        <button style={{ marginRight: "10px" }}>random</button>
+        <button style={{ marginRight: "10px" }}>select</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
