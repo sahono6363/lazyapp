459518bc16ab7f370538c26f15309d6808869f24
sahono6363 <mumi6363n@gmail.com>
2025-05-23 19:19:22 +0900

    チェックボタン修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import GoButton from "./Input/GoButton";
 import Input from "./Input/Input";
@@ -40,7 +40,11 @@ function App() {
       <div>
         {list.map((item, i) => (
           <div key={i} style={{ display: "flex" }}>
-          　<input
+            <div className="category2">{Categories[item.category].icon}</div>
+            <div className="title2">{item.title}</div>
+            <div className="from2">{item.from}</div>
+            <input
+              className="checkbox"
               type="checkbox"
               checked={item.checked}
               onChange={() => {
@@ -49,13 +53,9 @@ function App() {
                 setlist(newList);
               }}
             />
-            <div className="category2">{Categories[item.category].icon}</div>
-            <div className="title2">{item.title}</div>
-            <div className="from2">{item.from}</div>
             <button className="button2" onClick={() => handleDelete(i)}>
               <DeleteIcon />
             </button>
-
           </div>
         ))}
       </div>
